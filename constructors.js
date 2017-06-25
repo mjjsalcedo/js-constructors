8b2cd660b1865c07f3638908f355fff99c626e29
Mariel Salcedo <Mariel Salcedo>
2017-06-24 20:41:18 -1000

    spellcaster - spendmana

--- constructors.js
+++ constructors.js
@@ -79,8 +79,12 @@
  * @method  invoke
  */
 
-
-
+  function Spellcaster(name, health, mana){
+    this.name = name;
+    this.health = health;
+    this.mana = mana;
+    this.isAlive = true;
+  }
   /**
    * @method inflictDamage
    *
@@ -92,6 +96,16 @@
    * @param  {number} damage  Amount of damage to deal to the spellcaster
    */
 
+  Spellcaster.prototype.inflictDamage = function(damage){
+    if (this.health > damage){
+        this.health -= damage;
+      } else {
+        this.health = 0;
+        this.isAlive = false;
+      }
+   };
+
+
   /**
    * @method spendMana
    *
@@ -101,6 +115,15 @@
    * @param  {number} cost      The amount of mana to spend.
    * @return {boolean} success  Whether mana was successfully spent.
    */
+
+   Spellcaster.prototype.spendMana = function(cost){
+     if(this.mana > cost){
+        this.mana -= cost;
+        return true;
+     } else {
+        return false;
+     }
+   };
 
   /**
    * @method invoke
